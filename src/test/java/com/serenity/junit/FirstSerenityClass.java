37d99e35da141a0beffc495f5d7c758a9c383f60
igor.ferro.parreira@gmail.com <igor.ferro.parreira@gmail.com>
2020-07-25 16:40:34 -0300

    Tests outcomes in Serenity BDD

--- src/test/java/com/serenity/junit/FirstSerenityClass.java
+++ src/test/java/com/serenity/junit/FirstSerenityClass.java
@@ -3,9 +3,16 @@ package com.serenity.junit;
 import io.restassured.RestAssured;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Manual;
+import net.thucydides.core.annotations.Pending;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 @RunWith(SerenityRunner.class)
 public class FirstSerenityClass {
@@ -26,4 +33,43 @@ public class FirstSerenityClass {
                 .all()
                 .statusCode(200);
     }
+    @Test
+    public void getAllFail(){
+        //RestAssured.given()
+        SerenityRest.given()
+                .when()
+                .get("/image/random")
+                .then()
+                .log()
+                .all()
+                .statusCode(500);
+    }
+    @Pending
+    @Test
+    public  void pendingTest(){
+
+    }
+    @Ignore
+    @Test
+    public  void ignoreTest(){
+
+    }
+    @Test
+    public  void ErrorTest(){
+        System.out.println("This is a error test" + (5/0));
+
+    }
+
+    @Test
+    public  void fileDoesNotExistTest() throws FileNotFoundException {
+       File file = new File("E://file.txt");
+        FileReader fr = new FileReader(file);
+    }
+    @Manual
+    @Test
+    public  void manualTest(){
+
+    }
+
+
 }
