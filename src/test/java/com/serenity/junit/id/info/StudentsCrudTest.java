6bbd13b8fc5a08d500c9f21cc6b520543d8c2ced
igor.ferro.parreira@gmail.com <igor.ferro.parreira@gmail.com>
2020-07-27 06:31:49 -0300

    create list,update and delete tests

--- src/test/java/com/serenity/junit/id/info/StudentsCrudTest.java
+++ src/test/java/com/serenity/junit/id/info/StudentsCrudTest.java
@@ -7,23 +7,30 @@ import io.restassured.http.ContentType;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Title;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 @RunWith(SerenityRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StudentsCrudTest extends TestBase {
 
     static String firstName = "IGOR_USER"+TestUtils.getRandomValue();
     static String lastName = "IGOR_USER"+TestUtils.getRandomValue();
     static String programme = "ComputerScience";
     static String email = TestUtils.getRandomValue()+"IGOR_USER@email.com";
-
+    static int studentId;
 
     @Title("This will create a new student")
     @Test
-    public void createStudent(){
+    public void test001(){
         ArrayList<String> courses = new ArrayList<>();
         courses.add("Java");
         courses.add("C++");
@@ -35,7 +42,7 @@ public class StudentsCrudTest extends TestBase {
         student.setProgramme(programme);
         student.setCourses(courses);
 
-        SerenityRest.given()
+        SerenityRest.rest().given()
                 .contentType(ContentType.JSON)
                 .log()
                 .all()
@@ -46,6 +53,93 @@ public class StudentsCrudTest extends TestBase {
                 .log()
                 .all()
                 .statusCode(201);
+
+    }
+    @Title("Verify if the student was added to the application")
+    @Test
+    public void test002(){
+
+        String p1 = "findAll{it.firstName=='";
+        String p2= "'}.get(0)";
+
+        HashMap<String,Object> value = SerenityRest.given()
+                .when()
+                .get("http://localhost:8085/student/list")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .extract()
+                .path(p1+firstName+p2);
+
+        System.out.println("The value is: "+value);
+
+        assertThat(value,hasValue(firstName));
+        studentId = (int) value.get("id");
+    }
+    @Title("Update the user information and verify the updated information")
+    @Test
+    public void test003(){
+
+        String p1 = "findAll{it.firstName=='";
+        String p2= "'}.get(0)";
+
+        ArrayList<String> courses = new ArrayList<>();
+        courses.add("Java");
+        courses.add("C++");
+
+        firstName=firstName+"_Updated";
+
+        StudentClass student = new StudentClass();
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        student.setEmail(email);
+        student.setProgramme(programme);
+        student.setCourses(courses);
+
+        SerenityRest.rest().given()
+                .contentType(ContentType.JSON)
+                .log()
+                .all()
+                .when()
+                .body(student)
+                .put("http://localhost:8085/student/"+studentId)
+                .then()
+                .log()
+                .all();
+
+        HashMap<String,Object> value = SerenityRest.given()
+                .when()
+                .get("http://localhost:8085/student/list")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .extract()
+                .path(p1+firstName+p2);
+
+        System.out.println("The value is: "+value);
+
+        assertThat(value,hasValue(firstName));
+    }
+    @Title("Delete the student an verify if student deleted!")
+    @Test
+    public void test004(){
+        SerenityRest
+                .rest()
+                .given()
+                .when()
+                .delete("http://localhost:8085/student/"+studentId);
+
+        SerenityRest
+                .rest()
+                .given()
+                .when()
+                .get("http://localhost:8085/student/"+studentId).then()
+                .log()
+                .all()
+                .statusCode(404);
+
 
     }
 
